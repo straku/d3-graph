ac3c68328d2fd3a732cc50b1ef3cb14c06eb021d
Łukasz Strączyński <l.straczynski@hotmail.com>
2016-01-10 20:30:26 +0100

    Code style changes in physics.js

--- src/js/physics.js
+++ src/js/physics.js
@@ -1,18 +1,18 @@
 import d3 from 'd3'
 
-function singleCollide(node) {
-  let r = node.radius,
-    nx1 = node.x - r,
-    nx2 = node.x + r,
-    ny1 = node.y - r,
-    ny2 = node.y + r
+function singleCollide (node) {
+  const r = node.radius
+  const nx1 = node.x - r
+  const nx2 = node.x + r
+  const ny1 = node.y - r
+  const ny2 = node.y + r
 
   return function (quad, x1, y1, x2, y2) {
     if (quad.point && (quad.point !== node)) {
-      let x = node.x - quad.point.x,
-        y = node.y - quad.point.y,
-        l = Math.sqrt(x * x + y * y),
-        r = node.radius + quad.point.radius
+      const r = node.radius + quad.point.radius
+      let x = node.x - quad.point.x
+      let y = node.y - quad.point.y
+      let l = Math.sqrt(x * x + y * y)
 
       if (l < r) {
         l = (l - r) / l * .5
@@ -26,19 +26,19 @@ function singleCollide(node) {
   }
 }
 
-export function collide(nodes) {
-  let q = d3.geom.quadtree(nodes),
-    i = 0,
-    n = nodes.length
+export function collide (nodes) {
+  const q = d3.geom.quadtree(nodes)
+  const n = nodes.length
+  let i = 0
 
   while (++i < n) {
     q.visit(singleCollide(nodes[i]))
   }
 }
 
-export function position(d, width, height) {
-  let x = Math.max(d.radius, Math.min(width - d.radius, d.x)),
-    y = Math.max(d.radius, Math.min(height - d.radius, d.y))
+export function position (d, width, height) {
+  const x = Math.max(d.radius, Math.min(width - d.radius, d.x))
+  const y = Math.max(d.radius, Math.min(height - d.radius, d.y))
 
   return {
     x, y, transform: `translate(${x},${y})`
